80469ba34ecdfd5166e02b37bec7d16e8b087953
MargaritaMonogarova <margarita.monogarova@altenar.com>
2025-11-11 18:01:34 +0300

    Project init

--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\UsersDemoController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/demo/users/offset', [UsersDemoController::class, 'offset']);
-Route::get('/demo/users/cursor', [UsersDemoController::class, 'cursor']);
-Route::get('/demo/users/hybrid', [UsersDemoController::class, 'hybrid']);
